6861fe3a94cdff109100d100263efc8ca74e3fac
EddieBissellSimmons <144965190+EddieBissellSimmons@users.noreply.github.com>
2025-04-17 00:55:21 +0000

    done!

--- src/components.jsx
+++ src/components.jsx
@@ -1,23 +1,53 @@
-import {fahrenheitToCelsius, hello} from './functions.js';
+import {colour_mix, largest_product, day_of_the_week, pay_raise, is_leap} from './functions.js';
 
 function Question1(){
-    return <section>
-1. Write a function that takes a _name_ as an argument and _returns a string_ that _contains "hello"_ and the name paramerer. Test at least 3 names. <br />*Note ... I solved this one already
-      <h2>results</h2>
-      <p>hello("Rich") == "{hello('Rich')}"</p>
-      <p>hello("Bill") == "{hello('Bill')}"</p>
-      <p>hello("Chris") == "{hello('Chris')}"</p>
-    </section>;
-}
-
-function Question2(){
-    return <section>
-2. Write a function that converts from fahrenheit to celsius        
+    return<section>
+1. Wriate an rgb colour function.
 <h2>results</h2>
-<p>fahrenheitToCelsius(32) == "{fahrenheitToCelsius(32)}"</p>
-<p>fahrenheitToCelsius(212) == "{fahrenheitToCelsius(212)}"</p>
-<p>fahrenheitToCelsius(70) == "{fahrenheitToCelsius(70)}"</p>
+<p>colour_mix("red","green") == "{colour_mix('red','green')}"</p>
+<p>colour_mix("green","blue") == "{colour_mix('green','blue')}"</p>
+<p>colour_mix("blue","red") == "{colour_mix('blue','red')}"</p>
     </section>
 }
 
-export {Question1, Question2}
+function Question2(){
+    return<section>
+2. Write a function that takes three numbers and finds the product of the two greatness numbers.
+<h2>results</h2>
+<p>largest_product(1,2,3) == "{largest_product(1,2,3)}"</p>
+<p>largest_product(50,12,100) == "{largest_product(50,12,100)}"</p>
+<p>largest_product(69,10,32) == "{largest_product(69,10,32)}"</p>
+    </section>
+}
+
+function Question3(){
+    return<section>
+3. Write a function that tells you the day ofg the week based on the number.
+<h2>results</h2>
+<p>day_of_the_week(1) == "{day_of_the_week(1)}"</p>
+<p>day_of_the_week(3) == "{day_of_the_week(3)}"</p>
+<p>day_of_the_week(7) == "{day_of_the_week(7)}"</p>
+    </section>
+}
+
+function Question4(){
+    return<section>
+4. Write a function that increases employees salary based on status and years worked at the company.
+<h2>results</h2>
+<p>pay_raise("P",5,50000) == "{pay_raise('P',5,50000)}"</p>
+<p>pay_raise("F",1,35000) == "{pay_raise('F',1,35000)}"</p>
+<p>pay_raise("P",11,80000) == "{pay_raise('P',11,80000)}"</p>
+    </section>
+}
+
+function Question5(){
+    return<section>
+5. Write a function that determines if a year is a leap year.
+<h2>results</h2>
+<p>is_leap(2025) == "{is_leap(2025)}"</p>
+<p>is_leap(2028) == "{is_leap(2028)}"</p>
+<p>is_leap(1700) == "{is_leap(1700)}"</p>
+    </section>
+}
+
+export {Question1, Question2, Question3, Question4, Question5}
--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,82 @@
-function hello(name){
-    return(`hello ${name}`);
+//exam question 1 - colour mixer
+function colour_mix(rgb_colour1, rgb_colour2){
+    let rgb_colour;
+    if (rgb_colour1 == "red" && rgb_colour2 == "green" || rgb_colour1 == "green" && rgb_colour2 == "red"){
+        rgb_colour = "yellow";
+    } else if (rgb_colour1 == "green" && rgb_colour2 == "blue" || rgb_colour1 == "blue" && rgb_colour2 == "green"){
+        rgb_colour = "aqua";
+    } else if (rgb_colour1 == "blue" && rgb_colour2 == "red" || rgb_colour1 == "red" && rgb_colour2 == "blue"){
+        rgb_colour = "fuchsia";
+    } else {
+        rgb_colour = "error";
+    }
+    return rgb_colour;
 }
 
-function fahrenheitToCelsius(fahrenheit){
-    const celsius = (fahrenheit -32) * 5/9
-    return celsius;
+//exam question 2 - largest product
+function largest_product(val1,val2,val3){
+    let product;
+    if (val2 > val1 && val3 > val1){
+        product = val2 * val3;
+    } else if (val1 > val2 && val3 > val2){
+        product = val1 * val3;
+    } else if (val2 > val3 && val3 > val3){
+        product = val1 * val2;
+    }
+    return product;
 }
 
-export {hello, fahrenheitToCelsius}
+//exam question 3 - day of the week
+function day_of_the_week(day_num){
+    let day;
+    if (day_num == 1){
+        day = "Sunday";
+    } else if (day_num == 2){
+        day = "Monday";
+    } else if (day_num == 3){
+        day = "Tuesday";
+    } else if (day_num == 4){
+        day = "Wednesday";
+    } else if (day_num == 5){
+        day = "Thursday";
+    } else if (day_num == 6){
+        day = "Friday";
+    } else if (day_num == 7){
+        day = "Saturday";
+    } else {
+        day = "Error";
+    }
+    return day;
+}
+
+//exam question 4 - pay raise
+function pay_raise(status, years, salary){
+    let new_salary;
+    if (status = "F" && years >= 10){
+        new_salary = salary * 1.05;
+    } else if (status = "F" && years < 4){
+        new_salary = salary * 1.015;
+    } else if (status = "P" && years > 10){
+        new_salary = salary * 1.03;
+    } else if (status = "P" && years < 4){
+        new_salary = salary * 1.01;
+    } else {
+        new_salary = salary * 1.02;
+    }
+    return new_salary;
+}
+
+//exam question 5 - leap year
+function is_leap(year){
+    let result;
+    if (year % 4 === 0 && year % 100 === 0){
+        result = "false";
+    } else if (year % 4 === 0){
+        result = "true";
+    } else {
+        result = "false";
+    }
+    return result;
+}
+
+export {colour_mix, largest_product, day_of_the_week, pay_raise, is_leap}
